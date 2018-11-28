995517181b3fcc8caca07b965dcca55db291adb1
songheqi <songheqi1996@gmail.com>
2018-11-28 13:31:27 +0800

    Update BERT_NER.py

--- BERT_NER.py
+++ BERT_NER.py
@@ -4,6 +4,7 @@
 Copyright 2018 The Google AI Language Team Authors.
 BASED ON Google_BERT.
 @Author:zhoukaiyin
+Adjust code for chinese ner
 """
 from __future__ import absolute_import
 from __future__ import division
